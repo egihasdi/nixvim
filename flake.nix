fa40d9193c813ad9d730ae6fb0f76f08dbe8ee5d
egihasdi <egi.hasdi@gmail.com>
2024-11-04 02:52:16 +0700

    add system

--- flake.nix
+++ flake.nix
@@ -4,22 +4,30 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
+
+    systems.url = "github:nix-systems/x86_64-linux";
+
   };
 
-  outputs =
-    { nixpkgs, nixvim, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      nixvim' = nixvim.legacyPackages.${system};
-      nixvimModule = {
-        inherit pkgs;
-        module = import ./config;
-        extraSpecialArgs = {};
-      };
-      nvim = nixvim'.makeNixvimWithModule nixvimModule;
-    in
-    {
-      packages.${system}.default = nvim;
+  outputs = { nixpkgs, nixvim, systems, ... }:
+    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
+      packages = forEachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          nixvim' = nixvim.legacyPackages.${system};
+          nixvimModule = {
+            inherit pkgs;
+            module = import ./config;
+            extraSpecialArgs = { };
+          };
+          nvim = nixvim'.makeNixvimWithModule nixvimModule;
+        in { default = nvim; });
     };
+  #   let
+  #     
+  #   in { packages.${system}.default = nvim; };
+  # packages = let 
+  #   
+  #   in
 }
